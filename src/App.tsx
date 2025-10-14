051ba86ca3446797728703426ba0216649f61f8a
FarazZP <faraz@zeepalm.com>
2025-10-14 18:58:40 +0500

    last two pages added

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,12 @@ import {
   AdvanceQuestionPage,
   AnalyzingAnswerPage, InvestingStyleQuizPage, LeadPage, OptionBasedPage,
   QuestionPage, ScratchPage,
+  SabioIntroPage,
   TradingProfiles,
   TradingQuizExtraPage,
   TrustPage, WelcomePage,
-  YourTraderProfile
+  YourTraderProfile,
+  AnalyzingVid
 } from './pages';
 
 function App() {
@@ -16,6 +18,8 @@ function App() {
       <Routes>
         <Route path="/" element={<InvestingStyleQuizPage />} />
         <Route path="/welcome" element={<WelcomePage />} />
+        <Route path="/sabio-intro" element={<SabioIntroPage />} />
+        <Route path="/analyzing-final" element={<AnalyzingVid />} />
         <Route path="/analyzing" element={<AnalyzingAnswerPage />} />
         <Route path="/advance-question" element={<AdvanceQuestionPage />} />
         <Route path="/lead" element={<LeadPage />} />
