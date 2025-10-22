94d126446ae7a2a3a0a3c0ebfb0c431d7387ffb0
ShameelIrtaza-Zeepalm <shameelIrtaza@zeepalm.com>
2025-10-22 16:54:49 +0500

    Iphone Carousel

--- src/pages/LeadPage.tsx
+++ src/pages/LeadPage.tsx
@@ -15,18 +15,10 @@ const LeadPage: React.FC = () => {
                     </div>
                     {/* <EmailCaptureCard /> */}
                     <ChartCard />
-                    <div className='mt-6'> 
-                    <TabImageCarousel isPhone={true}/>
-                    
+                    <div className='mt-6'>
+                        <TabImageCarousel isPhone={true} />
                     </div>
-                    {/* <div className="mt-4" />
-                    <TestimonialCard />
-                    <PrimaryButton
-                        text="Unlock my trader profile"
-                        showIcon={false}
-                        onClick={handleOnClick}
-                    /> */}
-
+                    <div className='min-h-[200px]'/>
                 </div>
             </div>
 
