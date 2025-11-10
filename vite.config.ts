d82e8a299349b8957e5f27f2e132d413480a4612
ShameelIrtaza-Zeepalm <shameelIrtaza@zeepalm.com>
2025-11-10 11:44:17 +0500

    Bugs Fixed

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
+    outDir: process.env.BUILD_DIR || 'dist',
     chunkSizeWarningLimit: 1000, // increases warning threshold from 500 kB → 1000 kB
   },
 })
